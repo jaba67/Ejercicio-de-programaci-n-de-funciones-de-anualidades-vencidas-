d99776263e990af633246617affb86f524a81d77
jaba67 <2350483j@umich.mx>
2024-10-24 22:36:26 -0600

    Add files via upload

---
+++ funciones de anualidades vencidas.R
@@ -0,0 +1,54 @@
+# Se presentan la funciones para el calculo de anualidades vencidas
+# Autor: Jesus Adriel Bravo Aguilar 
+# V.1.0: 23/10/2024
+
+
+# formula VF(valor Futuro)
+
+VF=function(A, r, t){
+  exit=A*((1+r)^t-1)/r
+   return(exit)
+}
+
+# Fórmula para calcular la anualidad , conociendo valor futuro, tasa del periodo y número de pagos
+
+A=function(VF,r,Tper){
+  exit=(VF*r)/((1+r)^t-1)
+}
+
+# Fórmula para calcular el Número de pagos o plazo, conociendo valor futuro, anualidad y tasa del periodo
+
+t=function(VF,A,r){
+  exit=log((valor_futuro*tasa_periodo)/anualidad+1)/log(1+tasa_periodo)
+}
+
+# Fórmula para calcular la tasa del periodo, conociendo valor futuro, número de pagos y monto de la anualidad
+
+r= function(VF,A,Tper) {
+  exit=uniroot(function(r)A*(((1+r)^t-1)/r)-VF,lower=0,upper=1)$root
+}
+
+
+# Fórmula para calcular el valor actual, conociendo la anualidad, tasa de interés del periodo y el número (plazo) de anualidades.
+VA=function(A,r,Tper){
+  exit=A*(1-(1+r)^(-Tper))/r
+}
+
+# Fórmula para calcular la anualidad, conociendo valor actual, tasa del periodo y número de pagos
+
+A=function(VA,r,Tper){
+  exit=VA*(r/(1-(1+r)^(-Tper)))
+}
+
+# Fórmula para calcular el número de pagos o plazo, conociendo valor actual, anualidad y tasa del periodo
+
+t=function(VA,A,r){
+  exit=-log(1-(VA*r)/A)/log(1+r)
+}
+
+# Fórmula para calcular la tasa del periodo, conociendo valor actual, número de pagos y monto de la anualidad
+
+r=function(VA, A, Tper) {
+  exit=uniroot(function(r)A*(((1+r)^Tper-1)/r)-VA,lower=0,upper=1)$root
+  return(exit)
+}
